3b91a131c7cac066540ed9dbdc7bad67d02636da
Saroj Pedada <sarojpedada@gmail.com>
2022-12-24 21:49:43 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+
+mongoose.connect("mongodb+srv://yosj:MSairoj0@cluster0.maq2ed7.mongodb.net/?retryWrites=true&w=majority/todolistDB", {useNewUrlParser: true});
+
+const itemSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button too add a new item."
+});
+
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+app.get("/", function(req, res) {
+
+  Item.find({}, function(err, foundItems) {
+    if(foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DataBase")
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
+
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  })
+  item.save();
+  res.redirect("/");
+
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function(err) {
+    if(!err) {
+      console.log("Succesfully Removed Item");
+    } else {
+      console.log("Error 404!");
+    }
+  });
+  res.redirect("/");
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
